45cf66a74c069df13a848b1e350cadc1c9175f1d
you@example.com <you@example.com>
2021-07-09 14:42:54 +0530

    msg

--- aboota/celery.py
+++ aboota/celery.py
@@ -6,16 +6,16 @@ import django
 
 from celery import Celery
 
-os.environ.setdefault('DJANGOS_SETTINGS_MODULE', 'jrindia.settings')
+os.environ.setdefault('DJANGOS_SETTINGS_MODULE', 'aboota.settings')
 
-app = Celery('jrindia')
+app = Celery('aboota')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jrindia.settings.settings')
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../jrindia')))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aboota.settings.settings')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../aboota')))
 django.setup()
 from django.conf import settings
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
