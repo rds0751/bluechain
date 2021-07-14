d2926919d77c2076b07d84ae863e1a32c1cb8fec
you@example.com <you@example.com>
2021-07-14 19:03:28 +0530

    msg

--- kyc/admin.py
+++ kyc/admin.py
@@ -6,7 +6,7 @@ from .models import ImageUploadModel
 class kycAdmin(admin.ModelAdmin):
 	search_fields = ('user', )
 	list_display = [f.name for f in ImageUploadModel._meta.fields]
-	fields = ['aadhar_front', 'aadhar_back', 'pan_image', 'description', 'name', 'pan', 'approved', ]
-	readonly_fields = ( 'pan_image', 'aadhar_back', 'aadhar_front', )
+	fields = ['aadhar_front', 'aadhar_back', 'description', 'name', 'approved', ]
+	readonly_fields = ('aadhar_back', 'aadhar_front', )
 
 admin.site.register(ImageUploadModel, kycAdmin)
