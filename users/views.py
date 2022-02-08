72c9962bcb047c99c428ac31e86b9f8839be99fa
abc <abc>
2022-02-08 14:09:58 +0530

    msg

--- users/views.py
+++ users/views.py
@@ -303,12 +303,12 @@ class UserDashboardView(LoginRequiredMixin, ListView):
                 return_total = (rate*30)
             if return_total <= 0:
                 return_total = 0
-            wt = WalletHistory.objects.filter(user_id=self.request.user.username, comment="Sent to DCXa")
-            if wt.count == 0:
-                pass
-            else:
-                return_total = 0
         except Exception as e:
+            return_total = 0
+        wt = WalletHistory.objects.filter(user_id=self.request.user.username, comment="Sent to DCXa")
+        if wt.count == 0:
+            pass
+        else:
             return_total = 0
         
         context["amount"] = levelp
