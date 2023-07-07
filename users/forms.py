9a19bdd01ad741485477fe831c7552f7acb8606f
ripudaman <ripudaman@asynchain.com>
2023-07-07 16:12:30 +0530

    msg

--- users/forms.py
+++ users/forms.py
@@ -45,7 +45,11 @@ class SimpleSignupForm(SignupForm):
 			userr = 'blank'
 		if userr == 'blank':
 			referral = 'BNXG999999'
+<<<<<<< HEAD
 		plan = LevelUser()
+=======
+		plan = UserTotal()
+>>>>>>> 05d0820 (new commit)
 		plan.user = user
 		plan.level = LevelIncomeSettings.objects.get(id=9)
 		plan.active = False
