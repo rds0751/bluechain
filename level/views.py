91b36ebe10f3a11e751df14d1953014bc36b2d1d
ripudaman <ripudaman@asynchain.com>
2023-07-12 15:38:46 +0530

    hello

--- level/views.py
+++ level/views.py
@@ -505,22 +505,21 @@ def payment(request):
 
 def binary(request, plan):
     level = LevelIncomeSettings.objects.get(level=int(plan))
-    plan1 = PoolUser.objects.filter(user=request.user.username, plan=level, level=1)[0]
-    print(plan1)
-    # plan2 = PoolUser.objects.filter(user=request.user.username, plan=level, level=2)[0]
-    # plan3 = PoolUser.objects.filter(user=request.user.username, plan=level, level=3)[0]
+    plan1 = PoolUser.objects.filter(user=request.user.username, plan=level, level=1)
+    plan2 = PoolUser.objects.filter(user=request.user.username, plan=level, level=2)
+    plan3 = PoolUser.objects.filter(user=request.user.username, plan=level, level=3)
     downlines1 = PoolUser.objects.filter(upline=request.user.username, plan=level, level=1)
     downlines2 = PoolUser.objects.filter(upline=request.user.username, plan=level, level=2)
     downlines3 = PoolUser.objects.filter(upline=request.user.username, plan=level, level=3)
-    contex = {
+    context = {
         'plan1': plan1,
-        # 'plan2': plan2,
-        # 'plan3': plan3,
+        'plan2': plan2,
+        'plan3': plan3,
         'downlines1': downlines1,
         'downlines2': downlines2,
         'downlines3': downlines3
     }
-    return render(request,"binary/tree.html")
+    return render(request,"binary/tree.html", context)
 
 @csrf_exempt
 def payment_success(request):
