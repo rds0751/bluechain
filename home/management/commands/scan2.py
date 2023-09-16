bb183b337e402ab1ab22305a06158ba724e395f3
ripudaman <ripudaman@asynchain.com>
2023-09-16 14:35:16 +0530

    msg

--- home/management/commands/scan2.py
+++ home/management/commands/scan2.py
@@ -2,50 +2,126 @@ from django.core.management.base import BaseCommand
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
-from level.models import LevelUser
 from users.models import User
-from wallets.models import WalletHistory, PaymentOption
+from wallets.models import WalletHistory
 import datetime
+from datetime import timedelta
+from level.models import LevelIncomeSettings, LevelUser
+from users.models import User
 from django.utils import timezone
+from django.utils.crypto import get_random_string
+
 
 class Command(BaseCommand):
-    help = "Count Binary Data"
+	help = "Send ROI Incomes"
 
-    def handle(self, *args, **options):
-        users = LevelUser.objects.filter(active=True).order_by('user')
-        for user in users:
-            useru = User.objects.get(username=user)
-            wallet = useru.wallet
-            wallet9 = wallet - wallet*0.10*0.95
-            wallet10 = wallet - wallet*0.10*0.95
-            levelp = user
-            start_date = datetime.datetime.now() + datetime.timedelta(-26)
-            end_date = datetime.datetime.now() + datetime.timedelta(+4)
-            newusers = LevelUser.objects.filter(activated_at__range=(start_date, end_date))
-            if user in newusers:
-                wallet10 = wallet9
-            try:
-                plan_ends = levelp.activated_at
-                if plan_ends != 'gone' and plan_ends != 'not active':
-                    date_diff = plan_ends - timezone.now() - datetime.timedelta(days=4)
-                else:
-                    date_diff = 'blank'
-                if date_diff != 'blank':
-                    total_days = levelp.level.expiration_period * 30
-                    rate = levelp.level.return_amount/total_days
-                    if user in newusers:
-                        return_total = -(rate*(date_diff.days))*0.95
-                        # print(date_diff.days-2, rate, user)
-                    else:
-                        return_total = (rate*30)*0.95
-            except Exception as e:
-                raise e
-            try:
-                wallet = PaymentOption.objects.get(user=user)
-            except Exception as e:
-                wallet = 1
-            if wallet != 1:
-                print(str(useru)+', '+str(useru.email)+', '+', '+str(useru.mobile)+', '+', '+str(useru.name)+', '+str(return_total)+', '+str(wallet10)+', '+"{}".format(str(wallet.account_number))+', '+str(wallet.ifsc))
-            else:
-                print(str(useru)+', '+str(useru.email)+', '+', '+str(useru.mobile)+', '+', '+str(useru.name)+', '+str(return_total)+', '+str(wallet10))
+	def handle(self, *args, **options):
+		l = LevelUser.objects.filter(active=True)
+		def generateid():
+			txnid = get_random_string(8)
+			try:
+				txn = WalletHistory.objects.get(txnid = txnid)
+			except WalletHistory.DoesNotExist:
+				txn = 0
+			if txn:
+				generateid()
+			else:
+				return txnid
+			
+		def finduplines(user):  
+			try:
+				user = User.objects.get(username__iexact=str(user)) 
+				upline = user.referral   
+			except User.DoesNotExist:   
+				upline = 'blank'
+			return upline
+		
+		levels = {
+            'level1': 75/100,
+            'level2': 10/100,
+            'level3': 10/100,
+            'level4': 5/100,
+            'level5': 5/100,
+            'level6': 4/100,
+            'level7': 4/100,
+            'level8': 4/100,
+            'level9': 4/100,
+            'level10': 4/100,
+            'level11': 3/100,
+            'level12': 3/100,
+            'level13': 3/100,
+            'level14': 3/100,
+            'level15': 3/100,
+            'level16': 2/100,
+            'level17': 2/100,
+            'level18': 2/100,
+            'level19': 2/100,
+            'level20': 2/100
+			}
 
+		for x in l:
+			time_difference = timezone.now() - x.created_at
+			days_difference = time_difference.days
+			roi = x.level.amount * 0.05 / 30
+			wallet = WalletHistory()
+			wallet.comment = "Staking Incentive"
+			wallet.user_id = x.user
+			wallet.amount = roi
+			wallet.type = "credit"
+			wallet.save()
+			user = User.objects.get(username=x.user)
+			user.wallet += roi
+			user.save()
+			userid = User.objects.get(username=user.username)
+			level = 0
+			upline_user = userid.referral 
+			userid = user
+			amount = x.level.amount * 0.05 / 30
+			uplines = [upline_user, ]
+			while level < 20 and upline_user != 'blank':
+				upline_user = finduplines(str(upline_user))
+				uplines.append(upline_user)
+				level += 1
+
+
+			level = 0
+			for upline in uplines:
+				try:
+					upline_user = User.objects.get(username=upline) 
+				except Exception as e:  
+					upline_user = 'blank'
+				try:
+					upgraded = LevelUser.objects.get(user=upline, active=True)
+				except Exception as e:
+					upgraded = 'blank'
+				if upline_user != 'blank' and upgraded != 'blank':  
+					directs = LevelUser.objects.filter(direct=upline_user, active=True)
+					if user.referral == upline_user.username:
+						direct = True
+					else:
+						direct = False
+					upline_amount = levels['level{}'.format(level+1)]*amount
+					print(directs.count(), level, direct, days_difference)
+					if directs.count() >= level / 2:
+						upline_wallet = WalletHistory()   
+						upline_wallet.user_id = upline  
+						upline_wallet.amount = upline_amount    
+						upline_wallet.type = "credit"   
+						upline_wallet.comment = "New ROI by {} in level {}".format(user, level+1)
+						upline_wallet.txnid = generateid()
+						upline_user.wallet += upline_amount
+						upline_wallet.save()
+						upline_user.save()
+						print('if')
+					else:
+						upline_wallet = WalletHistory()
+						upline_wallet.user_id = upline
+						upline_wallet.amount = upline_amount
+						upline_wallet.type = "credit"
+						upline_wallet.comment = "{} ROI but Level {} not opened!".format(user, level+1)
+						upline_wallet.txnid = generateid()
+						upline_wallet.save()
+						print('else')
+				level += 1
+
+			print(user, wallet.amount)
