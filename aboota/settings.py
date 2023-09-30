1c5b2f3d1ab97574c0239c1d34ef97e378b1abbd
ripudaman <ripudaman@asynchain.com>
2023-09-30 18:54:23 +0530

    msg

--- aboota/settings.py
+++ aboota/settings.py
@@ -12,6 +12,10 @@
 
 import os
 from django.utils.translation import ugettext_lazy as _
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- level/views.py
+++ level/views.py
@@ -42,54 +42,54 @@ def leveltree(request, user, level):
         s = LevelUser.objects.get(user=user.username)
     except Exception as e:
         s = e
-    directs = LevelUser.objects.filter(direct=user, active=True).count()
+    directs = LevelUser.objects.filter(direct=user, active=False).count()
     level1 = LevelUser.objects.filter(direct=user.username).order_by('id')
     level1n = []
     for x in level1:
         level1n.append(x)
     level2n = []
     for y in level1n:
-        level2 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level2 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level2:
             level2n.append(z)
     level3n = []
     for y in level2n:
-        level3 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level3 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level3:
             level3n.append(z)
     level4n = []
     for y in level3n:
-        level4 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level4 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level4:
             level4n.append(z)
     level5n = []
     for y in level4n:
-        level5 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level5 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level5:
             level5n.append(z)
     level6n = []
     for y in level5n:
-        level6 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level6 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level6:
             level6n.append(z)
     level7n = []
     for y in level6n:
-        level7 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level7 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level7:
             level7n.append(z)
     level8n = []
     for y in level7n:
-        level8 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level8 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level8:
             level8n.append(z)
     level9n = []
     for y in level8n:
-        level9 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level9 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level9:
             level9n.append(z)
     level10n = []
     for y in level9n:
-        level10 = LevelUser.objects.filter(direct=y, active=True).order_by('id')
+        level10 = LevelUser.objects.filter(direct=y, active=False).order_by('id')
         for z in level10:
             level10n.append(z)
 
