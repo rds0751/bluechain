a2862f9dff08163ac769c2c3f342c688dc64a0c8
ripudaman <ripudaman@asynchain.com>
2023-10-13 18:15:29 +0530

    msg

--- level/views.py
+++ level/views.py
@@ -102,16 +102,11 @@ def leveltree(request, user, level):
         counting['{}'.format(level)] = len(a)
 
     levels = {  
-                'level1': 20/100,  
-                'level2': 10/100, 
-                'level3': 8/100, 
-                'level4': 8/100, 
-                'level5': 6/100, 
-                'level6': 5/100, 
-                'level7': 2/100, 
-                'level8': 1/100,
-                'level9': 0.5/100,
-                'level10': 0.5/100 
+                'level1': 2/100,  
+                'level2': 2/100, 
+                'level3': 2/100, 
+                'level4': 2/100, 
+                'level5': 2/100,
                 }
 
     business = {}
